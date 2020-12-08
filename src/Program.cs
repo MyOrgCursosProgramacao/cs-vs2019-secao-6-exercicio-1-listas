309c4f535fbefff9380a5198b3ecd6bb1e47d476
Diego <alvarodiego@gmail.com>
2020-12-08 18:59:40 -0300

    Relatório de funcionários e lógica de aumento de salário

--- src/Program.cs
+++ src/Program.cs
@@ -11,19 +11,18 @@ namespace src
             Console.WriteLine("Resolução do exercício proposto de listas da seção 6");
             Console.WriteLine("Entre com os dados dos funcionários");
 
-            Console.Write("Quantos funcionários desejas registrar? ");
+            Console.Write(Environment.NewLine + "Quantos funcionários desejas registrar? ");
             int numeroFuncionarios = int.Parse(Console.ReadLine());
 
             List<Funcionario> listaFuncionarios = new List<Funcionario>();
             int id;
             string nome;
             double salario;
-            double percentagem;
             bool loop = true;
 
-            for (int i=1; i <= numeroFuncionarios; i++)
+            for (int i = 1; i <= numeroFuncionarios; i++)
             {
-                Console.WriteLine($"Funcionário #{i}");
+                Console.WriteLine(Environment.NewLine + $"Funcionário #{i}");
                 Console.Write("Id: ");
                 id = int.Parse(Console.ReadLine());
                 do
@@ -47,12 +46,49 @@ namespace src
                 Console.Write("Nome: ");
                 nome = Console.ReadLine();
 
-                Console.WriteLine("Salário: R$ ");
+                Console.Write("Salário: R$ ");
                 salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
                 listaFuncionarios.Add(new Funcionario(id, nome, salario));
             }
 
+            Console.WriteLine(Environment.NewLine + "Lista atualizada de funcionários: ");
+            foreach (Funcionario obj in listaFuncionarios)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.Write(Environment.NewLine + "Digite a Id do funcionário que terá aumento de salário: ");
+            id = int.Parse(Console.ReadLine());
+            do
+            {
+                Funcionario func = listaFuncionarios.Find(x => x.Id == id);
+                if (func == null)
+                {
+                    Console.Write(Environment.NewLine
+                        + "Id de funcionário inexistente."
+                        + Environment.NewLine
+                        + "Digite a Id para o funcionário que terá aumento de salário: ");
+                    id = int.Parse(Console.ReadLine());
+                    loop = true;
+                }
+                else
+                {
+                    Console.WriteLine(func);
+                    Console.Write(Environment.NewLine + "Digite a percentagem de aumento de salário: ");
+                    func.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                    Console.WriteLine(Environment.NewLine + "Funcionário atualizado: " + func);
+                    loop = false;
+                }
+            } while (loop);
+
+            Console.WriteLine(Environment.NewLine + "Lista atualizada de funcionários: ");
+            foreach (Funcionario obj in listaFuncionarios)
+            {
+                Console.WriteLine(obj);
+            }
+
         }
+
     }
 }
