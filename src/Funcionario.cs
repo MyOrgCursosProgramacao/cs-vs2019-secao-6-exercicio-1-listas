a6e160d8c933afcc1012b858bb4926f288eee551
Diego <alvarodiego@gmail.com>
2020-12-08 18:11:25 -0300

    Classe criada

---
+++ src/Funcionario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace src
+{
+    class Funcionario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void AumentarSalario(double percentagem)
+        {
+            Salario = (1.0 + percentagem / 100.0) * Salario;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario;
+        }
+    }
+
+
+}
